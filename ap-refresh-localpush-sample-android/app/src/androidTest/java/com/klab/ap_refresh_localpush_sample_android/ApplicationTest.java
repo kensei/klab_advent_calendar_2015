7fa8286405c74425d91b934803a5eb72e6ac7981
kensei <kitahara-k@klab.com>
2015-12-01 15:54:16 +0800

    add brank androd studio project

---
+++ ap-refresh-localpush-sample-android/app/src/androidTest/java/com/klab/ap_refresh_localpush_sample_android/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.klab.ap_refresh_localpush_sample_android;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
